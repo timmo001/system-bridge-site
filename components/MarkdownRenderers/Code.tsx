6b34658ff2a67ac45c2e7604bbc0bf0dabe065e1
Aidan Timson <contact@timmo.xyz>
2022-05-05 12:40:18 +0100

    Use highlight js

--- components/MarkdownRenderers/Code.tsx
+++ components/MarkdownRenderers/Code.tsx
@@ -1,34 +1,26 @@
 import React, { ReactElement, useMemo } from "react";
 import { ReactMarkdownProps } from "react-markdown/lib/ast-to-react";
-import SyntaxHighlighter from "react-syntax-highlighter";
-import { atomOneDark } from "react-syntax-highlighter/dist/cjs/styles/hljs";
+import Highlight from "react-highlight";
+
+import "highlight.js/styles/atom-one-dark.css";
 
 function Code({
   className,
   children,
-  inline,
 }: JSX.IntrinsicElements["code"] &
   ReactMarkdownProps & {
     inline?: boolean;
   }): ReactElement {
+  const code = useMemo<string>(
+    () => String(children).replace(/\n$/, ""),
+    [children]
+  );
   const language = useMemo<string>(
     () => className?.replace("language-", ""),
     []
   );
-  // return !inline && language ? (
-  return (
-    <SyntaxHighlighter
-      style={atomOneDark}
-      customStyle={
-        inline && !language ? { display: "inline", padding: "0.2em" } : {}
-      }
-      language={language || "text"}
-      children={String(children).replace(/\n$/, "")}
-    />
-  );
-  // ) : (
-  //   <code {...props} className={className} children={children} />
-  // );
+
+  return <Highlight className={language}>{code}</Highlight>;
 }
 
 export default Code;
